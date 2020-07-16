fa504ac1c5d7b636a612cfbb2f3fe31af8fb2205
sohee <sorrri.b@gmail.com>
2020-07-16 19:34:18 +0900

    add neumorphic button

--- neumorphic-button/neumorphic-button/ContentView.swift
+++ neumorphic-button/neumorphic-button/ContentView.swift
@@ -10,7 +10,57 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack
+            {
+                Color.gray.edgesIgnoringSafeArea(.all)
+                Button(action: buttonAction) {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 25)
+                        Image(systemName: "wand.and.stars")
+                            .foregroundColor(Color.black.opacity(0.8))
+                            .font(.system(size: 100))
+                    }
+                }.buttonStyle(NeumorphicButtonStyle())
+        }
+    }
+    
+    func buttonAction() {
+        print("tapped!")
+    }
+}
+
+struct NeumorphicButtonStyle: ButtonStyle {
+    
+    let width: CGFloat = 260
+    let radius: CGFloat = 25
+    
+    func buttonUp(_ label: ButtonStyleConfiguration.Label) -> some View {
+        return label
+    }
+    
+    func makeBody(configuration: Self.Configuration) -> some View {
+        let isPressed = configuration.isPressed
+        return configuration.label
+            .foregroundColor(Color.gray.opacity(0.8))
+            .frame(width: width, height: width, alignment: .center)
+            .shadow(color: Color.white.opacity(isPressed ? 0 : 0.3), radius: 8, x: -15, y: -15)
+            .shadow(color: Color.black.opacity(isPressed ? 0 : 0.5), radius: 8, x: 15, y: 15)
+            .overlay(
+                RoundedRectangle(cornerRadius: radius)
+                    .stroke(Color.gray.opacity(isPressed ? 0.1 : 0), lineWidth: 18)
+                    .shadow(color: Color.black.opacity(isPressed ? 0.5 : 0), radius: 7, x: 15, y: 15)
+                    .clipShape(
+                        RoundedRectangle(cornerRadius: radius)
+                )
+        )
+            .overlay(
+                RoundedRectangle(cornerRadius: radius)
+                    .stroke(Color.gray.opacity(isPressed ? 0.1 : 0), lineWidth: 15)
+                    .shadow(color: Color.white.opacity(isPressed ? 0.45 : 0), radius: 9, x: -10, y: -10)
+                    .clipShape(
+                        RoundedRectangle(cornerRadius: radius)
+                )
+        )
     }
 }
 
